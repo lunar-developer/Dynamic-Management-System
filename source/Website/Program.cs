eaf74ab04cb0d2f04f58ed8f304f2e89bc75c322
Lunar <lunar.developer@gmail.com>
2016-11-02 23:33:32 +0700

    :package: Initial Version
    - Apply AspNet Core (.Net 4.6) & MVC 6
    - Reconstruct Project Structure:
    --> Rename StartUp class to Application (UseStartup)
    --> Rename wwwroot folder to Assets (UseWebRoot)
    --> Remove Bower (bower.json)
    --> Set output name for Project (Project.json)
    --> Fix bug cannot start debug when change output name for Project (Project.json)

---
+++ source/Website/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace LunarSoft.DMS.Website
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseWebRoot(Directory.GetCurrentDirectory() + "/Assets")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Application>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
